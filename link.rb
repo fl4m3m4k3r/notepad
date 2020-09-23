38eec62dadc9d5fa00d66069217351712e7170eb
flamemaker <fl4m3m4k3r@gmail.com>
2020-09-23 14:14:01 +0300

    post class + ancestors

---
+++ link.rb
@@ -0,0 +1,15 @@
+class Link < Post
+
+  def initialize
+    super
+
+    @url = ""
+  end
+
+  def read_from_consike
+  end
+
+  def to_strings
+  end
+  
+end
---
+++ memo.rb
@@ -0,0 +1,9 @@
+class Memo < Post
+
+  def read_from_consike
+  end
+
+  def to_strings
+  end
+
+end
